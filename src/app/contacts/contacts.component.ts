2cb150408b04c6de744691a8d6178037c0d80fe9
Wendell Wayne Estrada <wendell.estrada@irely.com>
2016-11-26 16:20:28 +0800

    add form controls

--- src/app/contacts/contacts.component.ts
+++ src/app/contacts/contacts.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FormBuilder, FormGroup, FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
     selector: 'dfp-contacts',
@@ -6,7 +7,93 @@ import { Component } from '@angular/core';
     templateUrl: './contacts.component.html'
 })
 export class ContactsComponent {
+    constructor(private fb: FormBuilder) {
+        this.contactForm = this.fb.group({
+            fullName: ["", Validators.required],
+            mobileNo: ["", Validators.required],
+            address: new FormControl()
+        });
+    }
+    
+    selectedContact: {};
+    contactForm: FormGroup;
+    
+    onContactClick(e, contact): void {
+        this.selectedContact = contact;
+        this.contactForm.patchValue(this.selectedContact);
+    }
+    
     contacts:any = [
+        {
+            fullName: 'Wendell Wayne H. Estrada',
+            mobileNo: '09476951219',
+            address: '26th Floor Tower 2 The Enterprise Center, Ayala Avenue cor Paseo de Roxas, Makati City'
+        },
+        {
+            fullName: 'Carline Key F. Capule',
+            mobileNo: '09329610621',
+            address: '16th Floor Tower 1 The Enterprise Center, Ayala Avenue cor Paseo de Roxas, Makati City'
+        },
+        {
+            fullName: 'Wendell Wayne H. Estrada',
+            mobileNo: '09476951219',
+            address: '26th Floor Tower 2 The Enterprise Center, Ayala Avenue cor Paseo de Roxas, Makati City'
+        },
+        {
+            fullName: 'Carline Key F. Capule',
+            mobileNo: '09329610621',
+            address: '16th Floor Tower 1 The Enterprise Center, Ayala Avenue cor Paseo de Roxas, Makati City'
+        },
+        {
+            fullName: 'Wendell Wayne H. Estrada',
+            mobileNo: '09476951219',
+            address: '26th Floor Tower 2 The Enterprise Center, Ayala Avenue cor Paseo de Roxas, Makati City'
+        },
+        {
+            fullName: 'Carline Key F. Capule',
+            mobileNo: '09329610621',
+            address: '16th Floor Tower 1 The Enterprise Center, Ayala Avenue cor Paseo de Roxas, Makati City'
+        },
+        {
+            fullName: 'Wendell Wayne H. Estrada',
+            mobileNo: '09476951219',
+            address: '26th Floor Tower 2 The Enterprise Center, Ayala Avenue cor Paseo de Roxas, Makati City'
+        },
+        {
+            fullName: 'Carline Key F. Capule',
+            mobileNo: '09329610621',
+            address: '16th Floor Tower 1 The Enterprise Center, Ayala Avenue cor Paseo de Roxas, Makati City'
+        },
+        {
+            fullName: 'Wendell Wayne H. Estrada',
+            mobileNo: '09476951219',
+            address: '26th Floor Tower 2 The Enterprise Center, Ayala Avenue cor Paseo de Roxas, Makati City'
+        },
+        {
+            fullName: 'Carline Key F. Capule',
+            mobileNo: '09329610621',
+            address: '16th Floor Tower 1 The Enterprise Center, Ayala Avenue cor Paseo de Roxas, Makati City'
+        },
+        {
+            fullName: 'Wendell Wayne H. Estrada',
+            mobileNo: '09476951219',
+            address: '26th Floor Tower 2 The Enterprise Center, Ayala Avenue cor Paseo de Roxas, Makati City'
+        },
+        {
+            fullName: 'Carline Key F. Capule',
+            mobileNo: '09329610621',
+            address: '16th Floor Tower 1 The Enterprise Center, Ayala Avenue cor Paseo de Roxas, Makati City'
+        },
+        {
+            fullName: 'Wendell Wayne H. Estrada',
+            mobileNo: '09476951219',
+            address: '26th Floor Tower 2 The Enterprise Center, Ayala Avenue cor Paseo de Roxas, Makati City'
+        },
+        {
+            fullName: 'Carline Key F. Capule',
+            mobileNo: '09329610621',
+            address: '16th Floor Tower 1 The Enterprise Center, Ayala Avenue cor Paseo de Roxas, Makati City'
+        },
         {
             fullName: 'Wendell Wayne H. Estrada',
             mobileNo: '09476951219',
